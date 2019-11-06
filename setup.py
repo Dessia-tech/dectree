c669c867d6cf83e9d94692c4cab43a86f1721953
Steven Masfaraud <masfaraud@dessia.tech>
2019-11-06 10:29:59 +0100

    Setup enhancement

--- setup.py
+++ setup.py
@@ -1,25 +1,65 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Mon Aug 20 12:21:31 2018
 
-@author: jezequel
 """
+
 from setuptools import setup
+
+from os.path import dirname, isdir, join
+import re
+from subprocess import CalledProcessError, check_output
+
 
 def readme():
     with open('README.rst') as f:
         return f.read()
 
+
+tag_re = re.compile(r'\btag: %s([0-9][^,]*)\b')
+version_re = re.compile('^Version: (.+)$', re.M)
+
+def get_version():
+    # Return the version if it has been injected into the file by git-archive
+    version = tag_re.search('$Format:%D$')
+    if version:
+        return version.group(1)
+
+    d = dirname(__file__)
+    
+    if isdir(join(d, '.git')):
+        cmd = 'git describe --tags'
+        try:
+            version = check_output(cmd.split()).decode().strip()[:]
+        except CalledProcessError:
+            raise RuntimeError('Unable to get version number from git tags')
+        if version[0]=='v':
+            version = version[1:]
+        # PEP 440 compatibility
+        if '-' in version:
+            future_version = version.split('-')[0].split('.')
+            if 'post' in future_version[-1]:
+                future_version = future_version[:-1]
+            future_version[-1] = str(int(future_version[-1])+1)
+            future_version = '.'.join(future_version)
+            number_commits = version.split('-')[1]
+            version = '{}.dev{}'.format(future_version, number_commits)
+            return version
+
+    else:
+        # Extract the version from the PKG-INFO file.
+        with open(join(d, 'PKG-INFO')) as f:
+            version = version_re.search(f.read()).group(1)
+                 
+    return version
+
 # Import dtree
 setup(name='dectree',
-      use_scm_version=True,
-      setup_requires=['setuptools_scm'],
-      description="Tools for decision trees",
+      version = get_version(),
+      description="A tool to explore decision trees",
       long_description='',
       keywords='',
-      url='https://github.com/Dessia-tech/dtree',
-#      cmdclass['register']=None,
+      url='https://github.com/Dessia-tech/dectree',
       author='DessIA',
       author_email='root@dessia.tech',
       packages=['dectree'],
